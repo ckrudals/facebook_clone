4baa7bd5bb2ebb74b1f8c5ec50cc5e9428ce73e1
KyungMin <2004ckck@naver.com>
2021-07-12 23:15:26 +0900

    [CREATE] viewPagerAdapter

---
+++ app/src/main/java/com/example/facebook_clone/viewPager/ViewPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.facebook_clone.viewPager
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.facebook_clone.MainActivity
+
+class ViewPagerAdapter(
+    mainActivity: MainActivity
+) :
+    FragmentStateAdapter(mainActivity) {
+
+    private var fragmentList: ArrayList<Fragment> = ArrayList()
+
+    override fun getItemCount() = fragmentList.size
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+
+    }
+
+    fun addFragment(fragment: Fragment) {
+        fragmentList.add(fragment)
+        notifyItemInserted (fragmentList.size - 1)
+    }
+
+}
